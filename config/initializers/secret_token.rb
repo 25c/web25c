df9bca27308566cf63f50566e9460d7133fffa97
Francis Li <mail@francisli.com>
2012-05-04 23:35:11 -0700

    Rails framework setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nkl::Application.config.secret_token = 'd7aa3f54a4cb16495626a9cace25ef579c319a41d423b0e0ae101306f4e86105459c37a7d0809b40280ca8debd8971c373f54f31b3a879be24cbdc3962c5894a'
