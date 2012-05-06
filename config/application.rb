44be75894bcb09b068e2b641cfd568c9a5413cda
Duane Barlow <duane.barlow@gmail.com>
2012-05-05 17:47:07 -0700

    making button.js precompiled separately

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,8 @@ module Nkl
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.precompile =+ ['button.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
