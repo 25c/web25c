9c33a4448f3ce301c53d4d4a1af4a237caa3b962
Francis Li <mail@francisli.com>
2012-05-21 23:58:49 -0700

    Patch cookie session store to serialize using JSON

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web25c::Application.config.secret_token = 'd7aa3f54a4cb16495626a9cace25ef579c319a41d423b0e0ae101306f4e86105459c37a7d0809b40280ca8debd8971c373f54f31b3a879be24cbdc3962c5894a'
+Web25c::Application.config.secret_token = 'c840c106474dfbeaf185447b436839c0d80a4bc5441ed78b7e1d816e2e47440502a6ff8fd3f85343e110548e7f814fdfddfc06e32fb5ff73090236cfbbe3b546'
