aaf371b817397c7f0e2cc0b0063e4e2473d1309a
Francis Li <mail@francisli.com>
2012-05-21 22:57:12 -0700

    Renaming nkl to web25c

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Nkl::Application.configure do
+Web25c::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Nkl::Application.config.session_store :cookie_store, key: '_nkl_session'
+Web25c::Application.config.session_store :cookie_store, key: '_web25c_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Nkl::Application.config.session_store :active_record_store
+# Web25c::Application.config.session_store :active_record_store
