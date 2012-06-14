871233e4159316d818322d7c58c9ffd42a3485f0
Francis Li <mail@francisli.com>
2012-06-14 14:33:58 -0700

    Precompile save-button.js

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Web25c
     # Enable the asset pipeline
     config.assets.enabled = true
     
-    config.assets.precompile += ['save-button.js.erb']
+    config.assets.precompile += ['save-button.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
