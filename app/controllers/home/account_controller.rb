ff5cb89c4f3b0ee75960a7eece00f5b69bb0c511
Lionel Jingles <lionel@leftronic.com>
2012-06-18 12:47:03 -0700

    adding more functionality to braintree integration

--- app/controllers/home/account_controller.rb
+++ app/controllers/home/account_controller.rb
@@ -9,6 +9,21 @@ class Home::AccountController < Home::HomeController
   end
   
   def confirm
+    @result = Braintree::TransparentRedirect.confirm(request.query_string)
+    if @result.success?
+      render :action => "confirm"
+    else
+      @user = current_user
+      @amount = calculate_amount
+      render :action => "jar"
+    end
+  end
+  
+  protected
+
+  def calculate_amount
+    # placeholder for code that calculates amount to be charged
+    "100.00"
   end
   
 end
