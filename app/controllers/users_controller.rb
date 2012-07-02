d6d641d1975bd7dd4c7f393dee16461b827f3f4b
Francis Li <mail@francisli.com>
2012-07-01 21:30:58 -0700

    Store referrer with tip click

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -141,7 +141,7 @@ class UsersController < ApplicationController
     # handle page redirecting
     if sign_in_successful
       if has_tip && !alert
-        redirect_to confirm_tip_path(:button_id => params[:button_id])
+        redirect_to confirm_tip_path(:button_id => params[:button_id], :referrer => params[:referrer])
       else
         redirect_to_session_redirect_path(home_dashboard_path)
       end
@@ -149,7 +149,7 @@ class UsersController < ApplicationController
     else
       @user = User.new if !@user
       if has_tip
-        redirect_to tip_path(:button_id => params[:button_id], :new => @new)
+        redirect_to tip_path(:button_id => params[:button_id], :referrer => params[:referrer], :new => @new)
       end
     end
   end
@@ -163,6 +163,7 @@ class UsersController < ApplicationController
     @user = User.new
     @new = params[:new] ? params[:new] == "true" : true
     @button_id = params[:button_id]
+    @referrer = params[:referrer]
     render :layout => "blank"
   end
   
@@ -171,7 +172,7 @@ class UsersController < ApplicationController
       @user = self.current_user
     else
       flash[:alert] = t('users.sign_in.failure')
-      redirect_to tip_path(:button_id => params[:button_id])
+      redirect_to tip_path(:button_id => params[:button_id], :referrer => params[:referrer])
       return
     end
     button = Button.find_by_uuid(params[:button_id])
@@ -180,6 +181,7 @@ class UsersController < ApplicationController
       click.uuid = UUID.new.generate
       click.user_id = @user.id
       click.button_id = button.id
+      click.referrer = params[:referrer]
       if click.save
         notice = t('users.sign_in.click_success')
       else
