e10d803095079f8ed923a33b9f370c6c45973da7
Lionel Jingles <lionel@leftronic.com>
2012-07-02 10:21:57 -0700

    adding default image url to production

--- config/initializers/05_paperclip.rb
+++ config/initializers/05_paperclip.rb
@@ -3,9 +3,11 @@ Paperclip.interpolates :uuid do |attachment, style|
 end
 
 if Rails.env.development?
-  Paperclip::Attachment.default_options[:default_url] = "http://mhpbooks.com/wp-content/uploads/2012/02/blank-profile-md.png"
+  Paperclip::Attachment.default_options[:default_url] = "http://s3.amazonaws.com/assets.plus25c.com/users/pictures/5833a440a697012f555f1231381359d0/original.png?1341249517"
   Paperclip::Attachment.default_options[:url] = "/s3/:class/:attachment/:uuid/:style.:extension"
 else
+  # temporary assigment of default image - need to replace with one we know will stay up
+  Paperclip::Attachment.default_options[:default_url] = "http://s3.amazonaws.com/assets.plus25c.com/users/pictures/5833a440a697012f555f1231381359d0/original.png?1341249517"
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_credentials] = AWS_SETTINGS
   Paperclip::Attachment.default_options[:bucket] = AWS_SETTINGS[:bucket]
