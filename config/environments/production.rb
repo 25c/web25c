1c67a6c96a903678cb1407ef24ecad08459bcf9b
Francis Li <mail@francisli.com>
2012-07-26 16:52:48 -0700

    Changing plus25c.com to 25c.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,10 +5,10 @@ Web25c::Application.configure do
     [u, p] == ['user25c', 'sup3rl!k3']
   end
   
-  ENV["API25C_URL"] = 'api.plus25c.com'
+  ENV["API25C_URL"] = 'api.25c.com'
 
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => "www.plus25c.com" }
+  config.action_mailer.default_url_options = { :host => "www.25c.com" }
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
