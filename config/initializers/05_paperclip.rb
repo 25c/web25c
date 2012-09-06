fb90dfd8e1048111e063a8600d0215d683c76a50
Lionel Jingles <lionel@leftronic.com>
2012-09-06 15:48:02 -0700

    making images and youtube embeds https

--- config/initializers/05_paperclip.rb
+++ config/initializers/05_paperclip.rb
@@ -3,13 +3,14 @@ Paperclip.interpolates :uuid do |attachment, style|
 end
 
 if Rails.env.development?
-  Paperclip::Attachment.default_options[:default_url] = "http://i50.tinypic.com/5y6ljo.png"
+  Paperclip::Attachment.default_options[:default_url] = "http://s3.amazonaws.com/assets.25c.com/users/pictures/no_pic.png"
   Paperclip::Attachment.default_options[:url] = "/s3/:class/:attachment/:uuid/:style.:extension"
 else
-  Paperclip::Attachment.default_options[:default_url] = "http://i50.tinypic.com/5y6ljo.png"
+  Paperclip::Attachment.default_options[:default_url] = "https://s3.amazonaws.com/assets.25c.com/users/pictures/no_pic.png"
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_credentials] = AWS_SETTINGS
   Paperclip::Attachment.default_options[:bucket] = AWS_SETTINGS[:bucket]
   Paperclip::Attachment.default_options[:url] = ":s3_path_url"
   Paperclip::Attachment.default_options[:path] = ":class/:attachment/:uuid/:style.:extension"
+  Paperclip::Attachment.default_options[:s3_protocol] = "https"
 end
