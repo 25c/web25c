6463694aafcb26d023ca427db517be9afeaa7d90
Lionel Jingles <lionel@leftronic.com>
2012-09-24 17:48:46 -0700

    adding default www subdomain to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Web25c::Application.routes.draw do
-    
+  
+  # endpoints for both 'tip' and 'www' domains
   match 'fb_share_callback' => 'home#fb_share_callback', :as => :fb_share_callback
   # hidden iframe page for ajax-like picture loading
   match 'home/upload_picture' => 'users#upload_picture', :as => :upload_picture
@@ -8,67 +9,70 @@ Web25c::Application.routes.draw do
     match 'update_button' => 'buttons#update_button', :as => :update_button, :via => :put
   end
   
-  # 'tip' subdomain routes
+  # wildcard routes for profiles under 'tip' domain
   match ':id' => 'users#show', :constraints => { :id => /.*/, :subdomain => 'tip' }, :as => :profile
   
   # 'www' subdomain routes
-  root :to => 'home#index'
-  
-  # match 'register' => 'users#new', :via => :get, :as => :register
-  # match 'register' => 'users#create', :via => :post
-  
-  match 'sign-in' => 'users#sign_in', :as => :sign_in
-  match 'sign-out' => 'users#sign_out', :as => :sign_out
-  match 'auth/dwolla/callback' => 'home/account#payout', :as => :dwolla_auth_callback
-  match 'auth/:provider/callback' => 'users#sign_in_callback'
-  
-  match 'about' => 'home#about', :as => :about
-  match 'faq' => 'home#faq', :as => :faq
-  match 'fees' => 'home#fees', :as => :fees
-  match 'terms' => 'home#terms', :as => :terms
-  match 'privacy' => 'home#privacy', :as => :privacy
-  
-  match 'tip/:button_id' => 'users#tip', :as => :tip
-  match 'blog/header' => 'home#blog_header'
-  match 'blog/footer' => 'home#blog_footer'
-  match 'paypal_process' => 'home#paypal_process'
-  
-  namespace :home do
-    # Buttons
-    resources :buttons
-    match 'get_button' => 'buttons#get_button', :as => :get_button
-    match 'receive_pledges' => 'buttons#receive_pledges', :as => :receive_pledges
-    match 'choose_pledge_message' => 'buttons#choose_pledge_message', :as => :choose_pledge_message
-        
-    # Dashboard
-    match '' => 'dashboard#index', :as => :dashboard
-    match 'undo_clicks' => 'dashboard#undo_clicks', :as => :undo_clicks
-    match 'delete_click' => 'dashboard#delete_click', :as => :delete_click, :via => :post
-    match 'process_clicks' => 'dashboard#process_clicks', :as => :process_clicks, :via => :post
-    
-    # Account
-    match 'payment' => 'account#payment', :as => :payment
-    match 'payment/success' => 'account#payment_success', :as => :payment_sucess
-    match 'payment/failure' => 'account#payment_failure', :as => :payment_failure
-    match 'payout' => 'account#payout', :as => :payout
-    match 'create_payment' => 'account#create_payment', :as => :create_payment, :via => :post
+  defaults :subdomain => Rails.env.development? ? nil : "www" do
+
+    root :to => 'home#index'
+
+    # match 'register' => 'users#new', :via => :get, :as => :register
+    # match 'register' => 'users#create', :via => :post
+
+    match 'sign-in' => 'users#sign_in', :as => :sign_in
+    match 'sign-out' => 'users#sign_out', :as => :sign_out
+    match 'auth/dwolla/callback' => 'home/account#payout', :as => :dwolla_auth_callback
+    match 'auth/:provider/callback' => 'users#sign_in_callback'
+
+    match 'about' => 'home#about', :as => :about
+    match 'faq' => 'home#faq', :as => :faq
+    match 'fees' => 'home#fees', :as => :fees
+    match 'terms' => 'home#terms', :as => :terms
+    match 'privacy' => 'home#privacy', :as => :privacy
+
+    match 'tip/:button_id' => 'users#tip', :as => :tip
+    match 'blog/header' => 'home#blog_header'
+    match 'blog/footer' => 'home#blog_footer'
+    match 'paypal_process' => 'home#paypal_process'
+
+    namespace :home do
+      # Buttons
+      resources :buttons
+      match 'get_button' => 'buttons#get_button', :as => :get_button
+      match 'receive_pledges' => 'buttons#receive_pledges', :as => :receive_pledges
+      match 'choose_pledge_message' => 'buttons#choose_pledge_message', :as => :choose_pledge_message
+
+      # Dashboard
+      match '' => 'dashboard#index', :as => :dashboard
+      match 'undo_clicks' => 'dashboard#undo_clicks', :as => :undo_clicks
+      match 'delete_click' => 'dashboard#delete_click', :as => :delete_click, :via => :post
+      match 'process_clicks' => 'dashboard#process_clicks', :as => :process_clicks, :via => :post
+
+      # Account
+      match 'payment' => 'account#payment', :as => :payment
+      match 'payment/success' => 'account#payment_success', :as => :payment_sucess
+      match 'payment/failure' => 'account#payment_failure', :as => :payment_failure
+      match 'payout' => 'account#payout', :as => :payout
+      match 'create_payment' => 'account#create_payment', :as => :create_payment, :via => :post
+    end
+
+    match 'home/account' => 'users#edit', :as => :home_account, :via => :get
+    match 'home/profile' => 'users#profile', :as => :home_profile
+    match 'home/choose_nickname' => 'users#choose_nickname', :as => :choose_nickname
+
+    namespace :admin do
+      resources :users, :except => [ :new, :create ]
+      match '' => 'dashboard#index', :as => :dashboard
+      match 'process_payment' => 'dashboard#process_payment', :as => :process_payment, :via => :post
+      match 'test' => 'dashboard#test', :as => :test
+    end
+
+    mount Resque::Server.new, :at => "/admin/resque/frame"
+
+    # profile wildcard
+    match ':id' => 'users#show', :constraints => { :id => /.*/ }
   end
-  
-  match 'home/account' => 'users#edit', :as => :home_account, :via => :get
-  match 'home/profile' => 'users#profile', :as => :home_profile
-  match 'home/choose_nickname' => 'users#choose_nickname', :as => :choose_nickname
-  
-  namespace :admin do
-    resources :users, :except => [ :new, :create ]
-    match '' => 'dashboard#index', :as => :dashboard
-    match 'process_payment' => 'dashboard#process_payment', :as => :process_payment, :via => :post
-    match 'test' => 'dashboard#test', :as => :test
-  end
-  
-  mount Resque::Server.new, :at => "/admin/resque/frame"
-  
-  # profile wildcard
-  match ':id' => 'users#show', :constraints => { :id => /.*/ }
   
   # catch-all 404 page for unknown routes
   match '*a' => 'home#not_found', :as => :not_found
