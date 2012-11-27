9d7c355abe955d243b8517a540440b829b9415a2
Francis Li <mail@francisli.com>
2012-11-27 12:36:46 -0800

    Updating test for removal of automatic first widget create

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,15 +2,4 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   
-  test "should create a button" do
-    user = User.create!(
-      :email => "test@test.com",
-      :password => "asdfasdf"
-    )
-    # force refresh from database
-    user.reload
-    # assert that a single default button has been created
-    assert_equal 1, user.buttons.size
-  end
-
 end
