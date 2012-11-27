81fd90ca83b9b41c9c75d26d9e7ebccdd718f99c
Francis Li <mail@francisli.com>
2012-11-27 13:26:22 -0800

    Setting up test testimonials widget, moving widget types into constant

--- app/models/button.rb
+++ app/models/button.rb
@@ -1,4 +1,6 @@
 class Button < ActiveRecord::Base
+  WIDGET_TYPES = %w(fan_belt testimonials)
+  
   belongs_to :user
   
   has_many :clicks
@@ -6,9 +8,10 @@ class Button < ActiveRecord::Base
     
   attr_accessible :title, :description, :pledge_message, :widget_type, :additional_parameters
   
-  serialize :share_users, JSON
-  
+  serialize :share_users, JSON  
   serialize :additional_parameters, JSON
+  
+  validates :widget_type, :inclusion => { :in => WIDGET_TYPES }
     
   before_create :generate_uuid
     
