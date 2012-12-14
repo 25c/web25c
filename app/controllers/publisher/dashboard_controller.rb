13249f3f5735906ccc799524b5691ecb3a68abc0
Francis Li <mail@francisli.com>
2012-12-13 16:48:25 -0800

    Support pagination

--- app/controllers/publisher/dashboard_controller.rb
+++ app/controllers/publisher/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Publisher::DashboardController < Publisher::PublisherController
 
   def index
     @clicks = self.current_user.clicks_received.order('created_at DESC').page params['page']
+    render :partial => 'clicks' if request.xhr?
   end
 
 end
