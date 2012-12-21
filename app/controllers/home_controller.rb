512786639db6c9375ffcd5ba71b8621163192c14
Lionel Jingles <lionel@leftronic.com>
2012-12-20 18:08:16 -0800

    fixing syntax issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   include ActiveMerchant::Billing::Integrations
   
-  skip_before_filter :show_placeholder_if_production, :only => :coming_soon, :demo
+  skip_before_filter :show_placeholder_if_production, :only => [ :coming_soon, :demo ]
   
   def coming_soon
   end
