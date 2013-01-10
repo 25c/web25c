883e7d5de8ca4e5fd003e4d4c60ed403d3ba6794
Lionel Jingles <lionel@leftronic.com>
2013-01-10 12:50:24 -0800

    dashboard updates

--- app/controllers/publisher/dashboard_controller.rb
+++ app/controllers/publisher/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Publisher::DashboardController < Publisher::PublisherController
   def index
     @clicks = self.current_user.clicks_received.order('created_at DESC').page params['page']
     render :partial => 'clicks' if request.xhr?
+    @total_received = Integer @clicks.sum { |click| click.amount_free + click.amount_paid }
   end
 
 end
